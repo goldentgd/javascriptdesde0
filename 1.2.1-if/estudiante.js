dbb22b7aca35cae434c9b26faa60c287ca14f883
Adriana Camarillo Mora <73756785+goldentgd@users.noreply.github.com>
2025-03-20 20:52:53 -0600

    Comparaciones y decisiones

---
+++ 1.2.1-if/estudiante.js
@@ -0,0 +1,11 @@
+let nota=95;
+
+if (nota >= 90){
+    console.log("Excelente");
+} else if (nota >= 75 && nota <= 89){
+    console.log("Bien")
+}else if (nota >= 60 && nota <= 74){
+    console.log("Suficiente")
+}else{
+    console.log("No aprueba")
+}
