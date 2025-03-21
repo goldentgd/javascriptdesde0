026073b803f93fc78d35ca46fa917034980de519
Adriana Camarillo Mora <73756785+goldentgd@users.noreply.github.com>
2025-03-20 21:17:34 -0600

    Agregar un condicional más

--- 1.2.1-if/estudiante.js
+++ 1.2.1-if/estudiante.js
@@ -9,3 +9,23 @@ if (nota >= 90){
 }else{
     console.log("El estudiante No aprueba")
 }
+
+let postre = "galletas";
+let bebida = "café";
+
+if(postre === "gelatina"){
+    console.log("Estoy en el hospital comiendo gelatina");
+}else if(postre === "pastel"){
+    console.log("Estoy en un cumpleaños comiendo pastel");
+}else if(postre ==="helado"){
+    console.log("Hace mucho calor por eso me compré un helado");
+}else if(postre === "frutas" && bebida === "agua"){
+    console.log("Estoy a dieta");
+}else if(postre === "galletas" && bebida === "café"){
+    console.log("Estoy en un funeral")
+}else if(postre === "lechuga" || bebida === "agua de limón"){
+    console.log("Soy vegetariana")
+}
+    else{
+    console.log("No hay postre para mí :(")
+}
