f38e4dfb9c82da56bc3224940684be4a72b77e24
Adriana Camarillo Mora <73756785+goldentgd@users.noreply.github.com>
2025-04-01 21:03:36 -0600

    Proyecto objetos

---
+++ Objetos/script.js
@@ -0,0 +1,36 @@
+function Libro(titulo, autor,anio,estado,capitulos){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anio = anio;
+    this.estado = estado;
+    this.capitulos = capitulos;
+    this.describirLibro = function (){
+        console.log(`Libro titulado "${this.titulo}", escrito por ${this.autor} en el año ${this.anio}, el estado es: ${this.estado}.`);
+    }
+}
+function agregarCapitulo(Libro, nuevoCapitulo) {
+    Libro.capitulos.push(nuevoCapitulo);
+    console.log(`Capítulo agregado: ${nuevoCapitulo}`);
+}
+
+function eliminarCapitulo(Libro, capitulo) {
+    const indice = Libro.capitulos.indexOf(capitulo);
+    if (indice !== -1) {
+        Libro.capitulos.splice(indice, 1);
+        console.log(`Capítulo eliminado: ${capitulo}`);
+    } else {
+        console.log(`El capítulo "${capitulo}" no se encontró en la lista.`);
+    }
+}
+
+let Libro1 = new Libro("Cumbres Borrascosas","Emily Brontë",1986,"disponible",["Capítulo 1", "Capítulo 2", "Capítulo 3"])
+Libro1.describirLibro();
+let Libro2 = new Libro("La sirenita","los hermanos",1962,"disponible",["Capítulo 1", "Capítulo 2", "Capítulo 3"])
+Libro2.describirLibro();
+let Libro3 = new Libro("Mulán","los hermanos",1952,"disponible",["Capítulo 1", "Capítulo 2", "Capítulo 3"])
+Libro3.describirLibro();
+agregarCapitulo(Libro1, "Capítulo 4");
+eliminarCapitulo(Libro1, "Capítulo 2");
+agregarCapitulo(Libro2, "Capítulo 5");
+eliminarCapitulo(Libro2, "Capítulo 3");
+
